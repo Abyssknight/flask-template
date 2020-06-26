3866eeebaa5bc5f4959d2eb7d5d37e4288c0c795
abyssknight <501835725@qq.com>
2020-06-26 12:29:03 +0800

    bug fix

--- flask_template/utils.py
+++ flask_template/utils.py
@@ -69,4 +69,4 @@ def get_host_ip():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(('8.8.8.8', 80))
         ip = s.getsockname()[0]
-        return ip
+    return ip
