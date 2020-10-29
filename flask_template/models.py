06bf9182bb5793d0b33e5dd054680e1a77faaab9
abyssknight <shixunguan@gmail.com>
2020-10-29 10:53:51 +0800

    add to_dict method to model object

--- flask_template/models.py
+++ flask_template/models.py
@@ -11,6 +11,9 @@ class BaseMixin:
     create_at = db.Column(db.DateTime, default=now, nullable=False, comment='创建时间')
     update_at = db.Column(db.DateTime, default=now, onupdate=now, nullable=False, comment='更新时间')
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 
 class MyDB(BaseMixin, db.Model):
     pass
